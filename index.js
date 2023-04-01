b1c23277cdfa8725ce094372983a98cd6fab95c1
Mukhtar Amin <mukhtaaramiin@gmail.com>
2023-04-01 09:50:41 +0300

    three variable added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 console.log("HELLO GITHUB");
 
-const Add = (a, b) => a + b;
+const cdd = (a, b) => a + b;
 console.log((Add(1,3)));
 const sub = (a, b) => a - b;
 console.log(sub(3,2));
+
+
+
+const a = "a"
+const b = "b"
+const c = "c"
